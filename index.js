00fe3cf32cf35659ac763a7ecfe2fb7988adcd57
Ilham <ilham.personal29@gmail.com>
2022-03-30 00:45:17 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   return res.send({
@@ -7,6 +8,6 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log(`Listening in port ${3000}`);
+app.listen(port, () => {
+  console.log(`Listening in port ${port}`);
 });
